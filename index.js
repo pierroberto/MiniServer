8cfe42a84d5952533ac7d286d56d8d650ccbfeb1
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-10-14 18:40:42 +0200

    tree structure created

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const router = require('./server/router.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
+app.listen(3000, () => console.log('listening...'));
+app.use(bodyParser());
+app.use(express.static('client'));
---
+++ server/router.js
@@ -0,0 +1,6 @@
+const Express = require('express');
+
+const router = new Express.Router();
+
+
+module.exports = router;
